75c01a31b77cc8e19e802f1c42cbef066299cfaf
jyk586 <54929465+jyk586@users.noreply.github.com>
2022-08-11 13:53:00 +0900

    Add files via upload
    route upload

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//http://127.0.0.1:8000/register  관리자 가입
+/*------------------------------------------
+--------------------------------------------
+All Normal Users Routes List
+--------------------------------------------
+--------------------------------------------*/
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+/*------------------------------------------
+--------------------------------------------
+All Admin Routes List
+--------------------------------------------
+--------------------------------------------*/
+//Route::middleware(['auth', 'user-access:admin'])->group(function () {
+
+//    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
+//});
+
+/*------------------------------------------
+--------------------------------------------
+All Admin Routes List
+--------------------------------------------
+--------------------------------------------*/
+//Route::middleware(['auth', 'user-access:manager'])->group(function () {
+
+//    Route::get('/manager/home', [HomeController::class, 'managerHome'])->name('manager.home');
+//});
