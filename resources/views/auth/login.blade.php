46fdfb092be710e0dda0442b07a9849704cbde69
LAPTOP-HRTNBNTI\droad <jyk586@gmail.com>
2022-08-29 14:09:12 +0900

    로그인 화면 작업 완료

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,73 @@
-@extends('layouts.app')
+<!DOCTYPE html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end"> ID (Email Address)</label>
-
-                            <div class="col-md-6">
-                                <input id="id" class="form-control" name="id" value="" required >
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<html lang="en" class="dark">
+<!-- BEGIN: Head -->
+<head>
+    <meta charset="utf-8">
+    <link href="/dist/images/logo.svg" rel="shortcut icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Midone admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Midone Admin Template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="LEFT4CODE">
+    <title>Login - Admin</title>
+    <!-- BEGIN: CSS Assets-->
+    <link rel="stylesheet" href="/dist/css/app.css" />
+    <!-- END: CSS Assets-->
+</head>
+<!-- END: Head -->
+<body class="login">
+<div class="container sm:px-10">
+    <div class="block xl:grid grid-cols-2 gap-4">
+        <!-- BEGIN: Login Info -->
+        <div class="hidden xl:flex flex-col min-h-screen">
+            <a href="" class="-intro-x flex items-center pt-5">
+                <img alt="Midone - HTML Admin Template" class="w-6" src="/dist/images/logo.svg">
+                <span class="text-white text-lg ml-3"> Rubick </span>
+            </a>
+            <div class="my-auto">
+                <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="/dist/images/illustration.svg">
+                <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">
+                    A few more clicks to
+                    <br>
+                    sign in to your account.
                 </div>
             </div>
         </div>
+        <!-- END: Login Info -->
+        <!-- BEGIN: Login Form -->
+        <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
+
+
+                <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
+                <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
+                    Sign In
+                </h2>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                <div class="intro-x mt-2 text-slate-400 xl:hidden text-center">A few more clicks to sign in to your account. </div>
+                <div class="intro-x mt-8">
+                    <input type="text" class="intro-x login__input form-control py-3 px-4 block" id="id" name="id" placeholder="Id">
+                    <input type="password" class="intro-x login__input form-control py-3 px-4 block mt-4" id="password" name="password" placeholder="Password">
+                </div>
+
+                <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
+                    <button class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top" type="submit">Login</button>
+                    <!--<button class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Register</button>-->
+                </div>
+                </form>
+                <div class="intro-x mt-10 xl:mt-24 text-slate-600 dark:text-slate-500 text-center xl:text-left"> By signin up, you agree to our <a class="text-primary dark:text-slate-200" href="">Terms and Conditions</a> & <a class="text-primary dark:text-slate-200" href="">Privacy Policy</a> </div>
+            </div>
+
+
+        </div>
+        <!-- END: Login Form -->
     </div>
 </div>
-@endsection
+
+
+<!-- BEGIN: JS Assets-->
+<script src="/dist/js/app.js"></script>
+<!-- END: JS Assets-->
+</body>
+</html>
+
