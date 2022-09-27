3b4b311aaf4d8451d1085e05ad0f7e6ba31a58ce
soungjae <rlatjdwowo20@gmail.com>
2022-09-27 16:21:32 +0900

    오류 수정

--- app/Service/ApiMemberServiceImpl.php
+++ app/Service/ApiMemberServiceImpl.php
@@ -358,7 +358,7 @@ class ApiMemberServiceImpl extends DBConnection  implements ApiMemberServiceInte
         $result = $this->statDB->table('members')
             ->where('email_id',$params['existingEmailId'])
             ->update([
-                'is_use' => 'N', 'mem_moddate' => \Carbon\Carbon::now()
+                'isuse' => 'N', 'mem_moddate' => \Carbon\Carbon::now()
             ]);
 
         return $result;
@@ -378,7 +378,7 @@ class ApiMemberServiceImpl extends DBConnection  implements ApiMemberServiceInte
             ->insert([
                 'name' => $params['name'], 'phone_number' => $params['phone_number'], 'email' => $params['email'], 'gubun' => $params['gubun'],
                 'nationality' => $params['nationality'], 'mem_nickname' => $params['mem_nickname'], 'marketing_consent' => $params['marketing_consent'],
-                'class' => 3,'mem_sanctions' => 0,'mem_status' => 'Y','mem_level' => 1,'mem_id' => $members_id, 'mem_regdate' => \Carbon\Carbon::now(),
+                'class' => 3,'mem_sanctions' => 0,'mem_status' => 1,'mem_level' => 1,'mem_id' => $members_id, 'mem_regdate' => \Carbon\Carbon::now(),
             ]);
 
         return $result;
