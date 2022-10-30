8934ec0b22f6842831b3bf03c7b4525e85d73add
DESKTOP-TIHB3UL\ygjung <jyk586@gmail.com>
2022-10-31 00:27:41 +0900

    상품정보 작업진행중

--- resources/views/products/productWrite.blade.php
+++ resources/views/products/productWrite.blade.php
@@ -183,7 +183,7 @@
                                                 <div class="editor">
                                                     <script type="text/javascript" src="/smarteditor2-2.8.2.3/js/HuskyEZCreator.js" charset="utf-8"></script>
                                                     <textarea class="form-control" name="information" id="information"
-                                                              rows="20" cols="10"
+                                                              rows="10" cols="10"
                                                               placeholder="내용을 입력해주세요"
                                                     ></textarea>
                                                 </div>
@@ -199,7 +199,7 @@
                                                 <div class="editor">
                                                     <script type="text/javascript" src="/smarteditor2-2.8.2.3/js/HuskyEZCreator.js" charset="utf-8"></script>
                                                     <textarea class="form-control" name="informationEng" id="informationEng"
-                                                              rows="20" cols="10"
+                                                              rows="10" cols="10"
                                                               placeholder="내용을 입력해주세요"
                                                     ></textarea>
                                                 </div>
@@ -215,27 +215,27 @@
                                             <tr>
                                                 <th class="whitespace-nowrap text-center">기본 배송비</th>
                                                 <td colspan="3" class="whitespace-nowrap">
-                                                    <input id="" type="text" class="form-control" placeholder="0">
+                                                    <input name="deliveryCharge" id="deliveryCharge" type="text" class="form-control" placeholder="0">
                                                 </td>
                                             </tr>
                                             <tr>
                                                 <th class="whitespace-nowrap text-center">반품 배송비</th>
                                                 <td>
-                                                    <input id="" type="text" class="form-control" placeholder="0">
+                                                    <input name="deliveryChargeRe" id="deliveryChargeRe" type="text" class="form-control" placeholder="0">
                                                 </td>
                                                 <th class="whitespace-nowrap text-center">교환 배송비 왕복</th>
                                                 <td>
-                                                    <input id="" type="text" class="form-control" placeholder="0">
+                                                    <input name="deliveryChargeCh" id="deliveryChargeCh" type="text" class="form-control" placeholder="0">
                                                 </td>
                                             </tr>
                                             <tr>
                                                 <th class="whitespace-nowrap text-center">제주도 배송비</th>
                                                 <td>
-                                                    <input id="regular-form-1" type="text" class="form-control" placeholder="0">
+                                                    <input name="deliveryChargeJj" id="deliveryChargeJj" type="text" class="form-control" placeholder="0">
                                                 </td>
                                                 <th class="whitespace-nowrap text-center">도서산간 베송비</th>
                                                 <td>
-                                                    <input id="regular-form-1" type="text" class="form-control" placeholder="0">
+                                                    <input name="deliveryChargeEx" id="deliveryChargeEx" type="text" class="form-control" placeholder="0">
                                                 </td>
                                             </tr>
                                         </table>
@@ -243,18 +243,32 @@
                                 </tr>
 
                                 <tr>
-                                    <th class="whitespace-nowrap text-center bg-primary/10">상세 설정</th>
+                                    <th class="whitespace-nowrap text-center bg-primary/10">정책 <br>배송/취소/교환/반품 </th>
                                     <td colspan="3">
                                         <div class="p-5" id="classic-editor">
                                             <div class="preview">
                                                 <div class="editor">
-                                                    <p>Content of the editor.</p>
+                                                    <script type="text/javascript" src="/smarteditor2-2.8.2.3/js/HuskyEZCreator.js" charset="utf-8"></script>
+                                                    <textarea class="form-control" name="policy" id="policy"
+                                                              rows="10" cols="10"
+                                                              placeholder="내용을 입력해주세요"
+                                                    ></textarea>
                                                 </div>
                                             </div>
-                                            <div class="source-code hidden">
-                                                <button data-target="#copy-classic-editor" class="copy-code btn py-1 px-2 btn-outline-secondary"> <i data-lucide="file" class="w-4 h-4 mr-2"></i> Copy example code </button>
-                                                <div class="overflow-y-auto mt-3 rounded-md">
-                                                    <pre class="source-preview" id="copy-classic-editor"> <code class="javascript"> import ClassicEditor from &quot;@ckeditor/ckeditor5-build-classic&quot;; $(&quot;.editor&quot;).each(function () { const el = this; ClassicEditor.create(el).catch((error) =HTMLCloseTag { console.error(error); }); }); </code> </pre>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th class="whitespace-nowrap text-center bg-primary/10"><label style="color:#ff0000;">(영어)</label>정책 <br> 배송/취소/교환/반품</th>
+                                    <td colspan="3">
+                                        <div class="p-5" id="classic-editor">
+                                            <div class="preview">
+                                                <div class="editor">
+                                                    <script type="text/javascript" src="/smarteditor2-2.8.2.3/js/HuskyEZCreator.js" charset="utf-8"></script>
+                                                    <textarea class="form-control" name="policyEng" id="policyEng"
+                                                              rows="10" cols="10"
+                                                              placeholder="내용을 입력해주세요"
+                                                    ></textarea>
                                                 </div>
                                             </div>
                                         </div>
@@ -289,16 +303,15 @@
                                 <tr>
                                     <th class="whitespace-nowrap text-center bg-primary/10">노출 상태</th>
                                     <td colspan="3">
-                                        <<select class="form-select w-56" aria-label=".form-select-lg example">
+                                        <select class="form-select w-56" aria-label=".form-select-lg example">
                                             <option>공개</option>
-                                            <option>전체1</option>
-                                            <option>전체2</option>
+                                            <option>비공개</option>
                                         </select>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th class="whitespace-nowrap text-center bg-primary/10">관리자</th>
-                                    <td colspan="3">조정훈</td>
+                                    <td colspan="3">홍길동</td>
                                 </tr>
                             </table>
                             <!-- 테이블 끝 -->
@@ -320,8 +333,10 @@
 
         <script>
 
-            let oEditors = []
-            let oEditors2 = []
+            let oEditors = [];
+            let oEditors2 = [];
+            let oEditors3 = [];
+            let oEditors4 = [];
 
             $(document).ready(function() {
                 nhn.husky.EZCreator.createInIFrame({
@@ -358,6 +373,45 @@
                     },
                 })
             })
+
+            $(document).ready(function() {
+                nhn.husky.EZCreator.createInIFrame({
+                    oAppRef: oEditors3,
+                    elPlaceHolder: "policy",
+                    sSkinURI: "/smarteditor2-2.8.2.3/SmartEditor2Skin.html",
+                    fCreator: "createSEditor2",
+                    htParams : {
+                        bUseToolbar : true,				// 툴바 사용 여부 (true:사용/ false:사용하지 않음)
+                        bUseVerticalResizer : true,		// 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+                        bUseModeChanger : true,			// 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+                        fOnBeforeUnload : function(){
+                        }
+                    },
+                    fOnAppLoad : function(){
+                    },
+                })
+            })
+
+            $(document).ready(function() {
+                nhn.husky.EZCreator.createInIFrame({
+                    oAppRef: oEditors4,
+                    elPlaceHolder: "policyEng",
+                    sSkinURI: "/smarteditor2-2.8.2.3/SmartEditor2Skin.html",
+                    fCreator: "createSEditor2",
+                    htParams : {
+                        bUseToolbar : true,				// 툴바 사용 여부 (true:사용/ false:사용하지 않음)
+                        bUseVerticalResizer : true,		// 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+                        bUseModeChanger : true,			// 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+                        fOnBeforeUnload : function(){
+                        }
+                    },
+                    fOnAppLoad : function(){
+                    },
+                })
+            })
+
+
+
 
         </script>
 
